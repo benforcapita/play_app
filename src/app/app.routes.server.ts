cee15fc8b9ea147c00f3fdd8c52c59400e36de09
Ben Blum <benforcapita@gmail.com>
2025-08-13 17:07:47 +0300

    make sure that character route is in the server and make the maximus scss

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,11 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
+// Avoid prerendering dynamic parameter routes; render them on the server instead.
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'characters/:id',
+    renderMode: RenderMode.Server
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
